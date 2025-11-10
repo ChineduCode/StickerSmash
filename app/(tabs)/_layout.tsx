3fdf815fb1e50c9abeb5fba46bb3ff4894f33774
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-10 17:42:57 +0100

    _layout tab index tab modified

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,20 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout(){
     return (
-        <Tabs>
-            <Tabs.Screen name='index' options={{ title: 'Home' }} />
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: '#ffd33d'
+            }}
+        >
+            <Tabs.Screen 
+                name='index' 
+                options={{ 
+                    title: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                    )
+                }} 
+            />
             <Tabs.Screen name='about' options={{ title: 'About' }} />
         </Tabs>
     );
