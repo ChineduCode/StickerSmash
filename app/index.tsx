fd3ffc93b4563b7b3f9c4dad75e8a7c67e352be6
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-10 16:24:20 +0100

    index page modified

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, StyleSheet, View } from "react-native";
+
+export default function Index(){
+    return (
+        <View>
+            <Text>Hello Worlds</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
