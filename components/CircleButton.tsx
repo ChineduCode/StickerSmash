d8a35c720c98d364187a607a4352087070f50bc2
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-10 23:13:41 +0100

    CircleButton created

---
+++ components/CircleButton.tsx
@@ -0,0 +1,11 @@
+import { View, StyleSheet, Text, Pressable } from "react-native";
+
+type Props = {}
+
+export default function CircleButton(){
+    return (
+        <View></View>
+    );
+}
+
+const styles = StyleSheet.create({});
