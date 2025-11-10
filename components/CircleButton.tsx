344df5e979af00c2f611064793f8e7dec0f22444
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-10 23:15:57 +0100

    CircleButton modified

--- components/CircleButton.tsx
+++ components/CircleButton.tsx
@@ -1,11 +1,30 @@
 import { View, StyleSheet, Text, Pressable } from "react-native";
 
-type Props = {}
+type Props = {
+    onPress: ()=> void;
+}
 
-export default function CircleButton(){
+export default function CircleButton({ onPress }: Props){
     return (
         <View></View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    circleButtonContainer: {
+        width: 84,
+        height: 84,
+        marginHorizontal: 60,
+        borderWidth: 4,
+        borderColor: '#ffd33d',
+        borderRadius: 42,
+        padding: 3,
+    },
+    circleButton: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 42,
+        backgroundColor: '#fff',
+    },
+});
