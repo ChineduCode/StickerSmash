bc7b122f723f0b6a6adefacccc95e1c1317681c1
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-10 21:54:22 +0100

    index screen modified by adding imagePicker functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
 import { Text, StyleSheet, View } from "react-native";
-import { Link } from "expo-router";
-import { Image } from "expo-image";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 import Button from "@/components/Button";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function IndexScreen(){
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images', 'videos'],
+            allowsEditing: true,
+            quality: 1,
+            videoQuality: 1
+        });
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
