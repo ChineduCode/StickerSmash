339061d7c8bab449149b4ee31b9607f0680e40e7
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-11 11:47:07 +0100

    index screen modified

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -64,7 +64,7 @@ export default function IndexScreen(){
                 </View>
             )}
             <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-                
+
             </EmojiPicker>
         </View>
     );
