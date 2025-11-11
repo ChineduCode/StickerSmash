35bd98939bba3120638e6fbee552212a4ff4df7f
ChineduCode <chinedunnaemeka2@gmail.com>
2025-11-11 11:36:56 +0100

    index screen modified

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,12 +6,14 @@ import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
 import IconButton from "@/components/IconButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function IndexScreen(){
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
